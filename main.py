0631b518f9bd15b1d8570b76457a3a12dc514e42
dahomaq35-afk <dahom.a.q35@gmail.com>
2026-09-24 04:11:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return 'Bot is alive!'
+
+
+def run():
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port)
+
+
+# تشغيل السيرفر الوهمي في الخلفية
+Thread(target=run).start()
+
+import os
 import discord
 from discord import app_commands
 from discord.ext import commands
